464f61ae027b63d2520d6a954d10cb4aac7a274f
David Wolever <david@wolever.net>
2013-08-23 14:28:30 -0400

    Remove Django imports from __init__.py

--- dwdj/__init__.py
+++ dwdj/__init__.py
@@ -1,8 +1,1 @@
-try:
-    from .shortcuts import r2r
-    from .views import redirect_next, guess_next
-except ImportError as e:
-    if "DJANGO_SETTINGS_MODULE" not in str(e):
-        raise
-
-__version__ = (0, 5, 0)
+__version__ = (0, 6, 0)
